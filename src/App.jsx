278c8bee81f0f5d9d35883d7dde42f1458682f1b
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-09 04:04:43 +0530

    render posts using a list of posts data - lesson 04 demonstrate rendering lists and keys in react

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,24 @@ import './App.css'
 import Post from "./components/Post.jsx";
 import Header from "./components/Header.jsx";
 
+// posts array
+const posts = [
+    {title: "Welcome", body: "Welcome to the blog zone."},
+    {title: "Lesson 01", body: "Welcome to Lesson 01."},
+    {title: "Lesson 02", body: "Welcome to Lesson 02."},
+    {title: "Lesson 03", body: "Welcome to Lesson 03."}
+]
+
 function App() {
 
     return (
         <>
             <Header name={"My blog zone"}/>
-            <Post title={"Welcome"} body={"Welcome to the blog zone."}/>
-            <Post title={"Lesson 01"} body={"Welcome to Lesson 01."}/>
-            <Post title={"Lesson 02"} body={"Welcome to Lesson 02."}/>
-            <Post title={"Lesson 03"} body={"Welcome to Lesson 03."}/>
+            {
+                posts.map((post, index) => (
+                    <Post key={index} title={post.title} body={post.body}/>
+                ))
+            }
         </>
     )
 }
