9c91ba3b8874882b5a60f1d5e23e4cee26c2a16b
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-09 02:28:31 +0530

    make "Header" component more complex - Lesson 03 make it more complex via adding search bar with an input field and a button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,21 @@
 * Stateless component for the header
 * */
 function Header(props) {
-  return (
-    <header>
-        <h1>{props.name}</h1>
-        <nav>
-            <ul>
-                <li><a href="/">Posts</a></li>
-                <li><a href="/new">New</a></li>
-            </ul>
-        </nav>
-    </header>
-  );
+    return (
+        <header>
+            <h1>{props.name}</h1>
+
+            <input type="text" placeholder="Search post"/>
+            <button>Search</button>
+
+            <nav>
+                <ul>
+                    <li><a href="/">Posts</a></li>
+                    <li><a href="/new">New</a></li>
+                </ul>
+            </nav>
+        </header>
+    );
 }
 
 export default Header;
