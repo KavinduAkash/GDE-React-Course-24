0bfe4f814089429dc5f67a30df6f227a230a77ae
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-09 01:53:57 +0530

    demonstrate jsx in "Post" component - lesson 03 keep post title and the body content in separate variables and use them in jsx

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -4,13 +4,16 @@
 
 import React from 'react';
 
+const title = "My first post";
+const body = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi commodi consequuntur culpa dicta " +
+    "doloremque eos excepturi illo, laborum non obcaecati, placeat quis quod, similique sint unde ut veniam. Laborum.";
+
 class Post extends React.Component {
   render() {
     return (
       <div>
-        <h1>Title</h1>
-        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ab animi commodi consequuntur culpa dicta doloremque
-            eos excepturi illo, laborum non obcaecati, placeat quis quod, similique sint unde ut veniam. Laborum.</p>
+        <h1>{title}</h1>
+        <p>{body}</p>
       </div>
     );
   }
