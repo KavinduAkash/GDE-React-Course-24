765df315dd5d31160a2de2effb98241b624588bf
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-10 03:07:28 +0530

    add more styles to nav bar - lesson 08

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -3,7 +3,7 @@ import {Link} from "react-router-dom";
 function NavBar () {
   return (
       <nav>
-          <ul style={{display: "flex", listStyle: "none"}}>
+          <ul style={{display: "flex", listStyle: "none", height: '100%', alignItems: 'center'}}>
               <li style={{margin: "0px 10px"}}><Link to="/">Posts</Link></li>
               <li style={{margin: "0px 10px"}}><Link to="/new">New</Link></li>
           </ul>
