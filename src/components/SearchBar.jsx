1d9fab9196b54f33517377af224bf48b14e031ef
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-09 08:53:55 +0530

    use "useState" react hook in "SearchBar" component - lesson 05 demonstrate how to use hooks in react stateless components

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,16 @@
+import {useState} from "react";
+
 function SearchBar() {
-  return (
-    <div>
-        <input type="text" placeholder="Search post"/>
-        <button>Search</button>
-    </div>
-  );
+    const[searchTerm, setSearchTerm] = useState(null);
+
+    return (
+        <div>
+            <input type="text" placeholder="Search post"
+                   onChange={e => setSearchTerm(e.target.value)}
+            />
+            <button disabled={!searchTerm}>Search</button>
+        </div>
+    );
 }
 
 export default SearchBar;
