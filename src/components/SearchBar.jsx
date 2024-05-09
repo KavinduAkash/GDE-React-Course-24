42db03c53133f20e427f9d3f49ac7c8e01513460
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-10 03:03:06 +0530

    apply react bootstrap to search bar - lesson 08

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,14 +1,22 @@
 import {useState} from "react";
+import {InputGroup, Form, Button} from "react-bootstrap";
 
 function SearchBar() {
     const[searchTerm, setSearchTerm] = useState(null);
 
     return (
         <div id={"search-bar"}>
-            <input type="text" placeholder="Search post"
-                   onChange={e => setSearchTerm(e.target.value)}
-            />
-            <button disabled={!searchTerm}>Search</button>
+            <InputGroup>
+                <Form.Control
+                    placeholder="Search post"
+                    aria-label="Search post"
+                    aria-describedby="basic-addon2"
+                    onChange={e => setSearchTerm(e.target.value)}
+                />
+                <Button disabled={!searchTerm} variant="outline-secondary" id="button-addon2">
+                    Search
+                </Button>
+            </InputGroup>
         </div>
     );
 }
