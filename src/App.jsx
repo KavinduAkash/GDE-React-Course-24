00cba731d366a9f1a6a14d0177b9d48ba2cf89cd
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-10 10:12:37 +0530

    add redux to user management - lesson 6

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,19 @@ import LoginView from "./views/LoginView.jsx";
 import PostView from "./views/PostView.jsx";
 import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import {Provider} from "react-redux";
+import {combineReducers, createStore} from "redux";
+import userReducer from "./redux/UserReducer.js";
 function App() {
+
+    const rootReducer = combineReducers({
+        UserReducer: userReducer, // Add more reducers as needed
+    });
+    const store = createStore(rootReducer);
 
     return (
         <>
+            <Provider store={store}>
             <BrowserRouter>
                 <Header name={"My blog zone"}/>
                 <Routes>
@@ -15,6 +24,7 @@ function App() {
                     <Route path={'/login'} element={<LoginView/>}/>
                 </Routes>
             </BrowserRouter>
+            </Provider>
         </>
     )
 }
--- src/views/LoginView.jsx
+++ src/views/LoginView.jsx
@@ -1,22 +1,49 @@
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 import "../assets/styles/login.css";
+import {useDispatch} from "react-redux";
+import {useState} from "react";
+import {logoutUser} from "../redux/UserActions.js";
+import {useNavigate} from "react-router-dom";
+
+const userList = [{id: 1, email: 'jhonedeo@gmail.com', password: '123'}, {
+    id: 2,
+    email: 'jackd@yahoo.com',
+    password: '1234'
+}];
 
 function LoginView() {
+
+    const dispatch = useDispatch();
+    const navigate = useNavigate();
+
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+
+    const login = () => {
+        let user = userList.filter(user => user.email == email && user.password == password);
+        if (user[0]) {
+            dispatch(logoutUser(user[0]));
+            navigate("/");
+        } else {
+            alert('Invalid email or password');
+        }
+    }
+
     return (
         <div>
             <Form id="login">
                 <h1 className={"text-center"}>Login</h1>
                 <Form.Group>
                     <Form.Label>Email</Form.Label>
-                    <Form.Control type="email" placeholder="Enter email" />
+                    <Form.Control type="email" placeholder="Enter email" onChange={e => setEmail(e.target.value)}/>
                 </Form.Group>
                 <Form.Group>
                     <Form.Label>Password</Form.Label>
-                    <Form.Control type="password" placeholder="Password" />
+                    <Form.Control type="password" placeholder="Password" onChange={e => setPassword(e.target.value)}/>
                 </Form.Group>
                 <div className={"text-center"}>
-                    <Button variant="primary" type="button">
+                    <Button variant="primary" type="button" onClick={login}>
                         Login
                     </Button>
                 </div>
