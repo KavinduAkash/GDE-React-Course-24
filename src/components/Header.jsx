292355502d5bdf5300aa20fc7b6c9400b5e7ce07
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-10 10:43:00 +0530

    update header with login and logout buttons

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,8 +3,23 @@
 * */
 import SearchBar from "./SearchBar.jsx";
 import NavBar from "./NavBar.jsx";
+import {useDispatch, useSelector} from "react-redux";
+import {useNavigate} from "react-router-dom";
+import {Button} from "react-bootstrap";
+import {logoutUser} from "../redux/UserActions.js";
 
 function Header(props) {
+
+    let navigate = useNavigate();
+    let dispatch = useDispatch();
+
+    const user = useSelector(state => state.user);
+
+    const logout = () => {
+        dispatch(logoutUser(null));
+        navigate("/login");
+    }
+
     return (
         <header id={"header"}>
             <h1>{props.name}</h1>
@@ -22,6 +37,13 @@ function Header(props) {
             {/*</nav>*/}
 
             <NavBar/>
+
+            <div id={"header-user"}>
+
+                {
+                    user ? <div>Welcome {user.email} | <Button variant="outline-danger" onClick={logout}>Logout</Button></div> : <div><Button variant="outline-primary" onClick={() => navigate("/login")}>Login</Button></div>
+                }
+            </div>
         </header>
     );
 }
