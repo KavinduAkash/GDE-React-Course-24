eadfaafeff5d7d14801be876c106007d392e787c
Kavindu Akash <bkkasamarasingha1220@gmail.com>
2024-05-13 03:53:19 +0530

    fetch post data via an api - lesson 10 fetch data from the json placeholder

--- src/views/PostView.jsx
+++ src/views/PostView.jsx
@@ -1,15 +1,31 @@
 import withLoadingIndicator from "../components/hoc/withLoadingIndicator.jsx";
 import Post from "../components/Post.jsx";
+import {useEffect, useState} from "react";
 
 // posts array
-const posts = [
-    {title: "Welcome", body: "Welcome to the blog zone."},
-    {title: "Lesson 01", body: "Welcome to Lesson 01."},
-    {title: "Lesson 02", body: "Welcome to Lesson 02."},
-    {title: "Lesson 03", body: "Welcome to Lesson 03."}
-]
+// const posts = [
+//     {title: "Welcome", body: "Welcome to the blog zone."},
+//     {title: "Lesson 01", body: "Welcome to Lesson 01."},
+//     {title: "Lesson 02", body: "Welcome to Lesson 02."},
+//     {title: "Lesson 03", body: "Welcome to Lesson 03."}
+// ]
 
 function PostView() {
+
+    const[posts, setPosts] = useState([]);
+
+    const getAllPosts = () => {
+        fetch('https://jsonplaceholder.typicode.com/posts')
+            .then(response => response.json())
+            .then(json => {
+                setPosts(json)
+            });
+    }
+
+    useEffect(() => {
+        getAllPosts()
+    }, [])
+
     return(
         <div>
             {
